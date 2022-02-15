125a61acb50f68067d5b627698ed01baf804c2fa
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-02-15 14:32:49 +0100

    Add files via upload
    + prise en compte du titre et chapo dans l'édition des articles

--- cssBackSlash/js/cssBackSlash.js
+++ cssBackSlash/js/cssBackSlash.js
@@ -1,8 +1,19 @@
-window.onload = function() {
-let txtA = document.querySelector('[name="content"]');
-let content = txtA.innerHTML;	
-function escapeRegex(string) {	
-    return string.replace(/[\\]/g, '\\$&');
-}
-txtA.innerHTML=  escapeRegex(content);  
-}
+(function () {
+    window.onload = function() {
+        let txtA = document.querySelectorAll('[name="title"],[name="chapo"],[name="content"]');
+        
+        for (i=0;i<txtA.length;i++){  
+          if(txtA[i].tagName == 'INPUT'){
+            let content = txtA[i].value;
+            txtA[i].value=  escapeRegex(content); 
+          }
+          else {  let content = txtA[i].innerHTML;
+            txtA[i].innerHTML=  escapeRegex(content); 
+          } 	            
+        }
+        function escapeRegex(string) {	
+            return string.replace(/[\\]/g, '\\$&');
+        }
+    }
+
+})();;
