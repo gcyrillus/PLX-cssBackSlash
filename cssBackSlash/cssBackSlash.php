fcaa5d0223ff754a0ab52be7c0ebb938dd16d22f
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2023-10-29 18:51:04 +0100

    Update cssBackSlash.php
    make it short & simple

--- cssBackSlash/cssBackSlash.php
+++ cssBackSlash/cssBackSlash.php
@@ -1,56 +1,33 @@
 <?php
-    if(!defined('PLX_ROOT')) {
-        die('Are you silly ?');
-    }
-
-    class cssBackSlash extends plxPlugin {
-        const HOOKS = array(
-            'AdminSettingsEdittplFoot',
-			'AdminStaticFoot',
-			'AdminArticleFoot',
-        );
-        const BEGIN_CODE = '<?php' . PHP_EOL;
-        const END_CODE = PHP_EOL . '?>';
-
-        public function __construct($default_lang) {
-            # appel du constructeur de la classe plxPlugin (obligatoire)
-            parent::__construct($default_lang);
-
-            # Ajoute des hooks
-            foreach(self::HOOKS as $hook) {
-                $this->addHook($hook, $hook);
-            }
-        }
-
+	if(!defined('PLX_ROOT')) {
+		die('does not run on its own');
+	}
+	
+	class cssBackSlash extends plxPlugin {
+		const HOOKS = array(
+		'AdminFootEndBody',
+		);
+		const BEGIN_CODE = '<?php' . PHP_EOL;
+		const END_CODE = PHP_EOL . '?>';
 		
-		#Ajoute un caractére d'échappement devant chaque \ trouvé dans une feuille de style
-        public function AdminSettingsEdittplFoot() {
-            echo self::BEGIN_CODE;
-?>
-$plgPlugin = $plxAdmin->plxPlugins->aPlugins['<?= __CLASS__ ?>'];
-if(substr($tpl, strrpos($tpl, '.') + 1) =='css')	echo  '<script src="'.PLX_PLUGINS . '<?= __CLASS__ ?>/js/<?= __CLASS__ ?>.js"></script>';
-<?php
-            echo self::END_CODE;
-        }	
+		public function __construct($default_lang) {
+			# appel du constructeur de la classe plxPlugin (obligatoire)
+			parent::__construct($default_lang);
+			
+			# Ajoute des hooks
+			foreach(self::HOOKS as $hook) {
+				$this->addHook($hook, $hook);
+			}
+		}
 		
-		#Ajoute un caractére d'échappement devant chaque \ trouvé dans une page statique
-        public function AdminStaticFoot() {
-            echo self::BEGIN_CODE;
-?>
-$plgPlugin = $plxAdmin->plxPlugins->aPlugins['<?= __CLASS__ ?>'];
-echo  '<script src="'.PLX_PLUGINS . '<?= __CLASS__ ?>/js/<?= __CLASS__ ?>.js"></script>';
-<?php
-            echo self::END_CODE;
-        }
+		#Ajoute un caractére d'échappement devant chaque \ trouvé dans un formulaire
+		public function AdminFootEndBody() {
+			echo self::BEGIN_CODE;
+		?>
+		$plgPlugin = $plxAdmin->plxPlugins->aPlugins['<?= __CLASS__ ?>'];
+		echo  '<script src="'.PLX_PLUGINS . '<?= __CLASS__ ?>/js/<?= __CLASS__ ?>.js"></script>';
+		<?php
+			echo self::END_CODE;
+		}	
 		
-		#Ajoute un caractére d'échappement devant chaque \ trouvé dans une page article
-        public function AdminArticleFoot() {
-            echo self::BEGIN_CODE;
-?>
-$plgPlugin = $plxAdmin->plxPlugins->aPlugins['<?= __CLASS__ ?>'];
-echo  '<script src="'.PLX_PLUGINS . '<?= __CLASS__ ?>/js/<?= __CLASS__ ?>.js"></script>';
-<?php
-            echo self::END_CODE;
-        }	
-		
-    }
+	}
